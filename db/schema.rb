77ddde7483e8c48d7967a37f054d7a018ca3f963
sanjanaballa <sanjana.balla@gmail.com>
2021-06-14 21:34:44 -0700

    create project user association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_15_023900) do
+ActiveRecord::Schema.define(version: 2021_06_15_031506) do
 
   create_table "arifacts", force: :cascade do |t|
     t.string "name"
@@ -83,6 +83,15 @@ ActiveRecord::Schema.define(version: 2021_06_15_023900) do
     t.integer "tenant_id", null: false
     t.integer "user_id", null: false
     t.index ["tenant_id", "user_id"], name: "index_tenants_users_on_tenant_id_and_user_id"
+  end
+
+  create_table "user_projects", force: :cascade do |t|
+    t.integer "project_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_user_projects_on_project_id"
+    t.index ["user_id"], name: "index_user_projects_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
